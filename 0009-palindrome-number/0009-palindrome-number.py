bf212548eac0042457a163edd9195b492a1c884d
MANGALI NAVYA <middenavya51@gmail.com>
2026-09-01 18:31:20 +0530

    Time: 16 ms (13.52%), Space: 19.4 MB (19.35%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        dup=int(x)
+        reverse=0
+        while x>0:
+            digit=x%10
+            reverse=(reverse*10)+digit
+            x=x//10
+        return dup==reverse
+        
